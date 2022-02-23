145a407af0a3973c59275796aa0e1d3988a8d268
ArtemFree <aparkin-art99@mail.ru>
2022-02-23 20:09:18 +0700

    Build

--- build/index.js
+++ build/index.js
@@ -1,6 +1,15 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const one_1 = require("./src/one");
-const user_module_1 = require("./src/user-module");
-const asd = 10;
-console.log("asdasd", user_module_1.myUser, one_1.base_age);
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.listen(5000, () => {
+    console.log("Server has started", 5000);
+});
+app.get("/start", (req, res) => {
+    res.json({
+        result: "successful result",
+    });
+});
